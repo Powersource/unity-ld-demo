3f53dcb9e7332b80ee7602a06415ee7104cb6797
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-04-03 03:46:55 +0200

    Create physics and start coding

---
+++ Assets/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	// Put visual-related stuff in here
+	void Update () {
+	
+	}
+
+	// This is called every physics tick.
+	// Remember to multiply eg. movement distance by Time.deltaTime
+	// to make the game run the same at different framerates.
+	// If you do you will be better at coding games than
+	// many professional studios.
+	void FixedUpdate()
+}
